26fbaf5039208baf499b3a33404fc935acd07490
rjgadonniex <100667821+rjgadonniex@users.noreply.github.com>
2024-03-27 22:52:13 -0400

    error fix

--- client/src/pages/AdoptableDogs.js
+++ client/src/pages/AdoptableDogs.js
@@ -8,7 +8,6 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-import doggy from './doggy.jpg'
 
 
 const AdoptableDogs = () => {
